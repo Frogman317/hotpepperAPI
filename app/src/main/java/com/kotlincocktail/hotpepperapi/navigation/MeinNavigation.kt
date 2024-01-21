0bf06867361718b462f381d4e665ec8170c2edf6
Y_Nishino <kaek33586@gmail.com>
2024-01-22 08:16:54 +0900

    add view navigation class

---
+++ app/src/main/java/com/kotlincocktail/hotpepperapi/navigation/MeinNavigation.kt
@@ -0,0 +1,50 @@
+package com.kotlincocktail.hotpepperapi.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.kotlincocktail.hotpepperapi.tool.HotpepperAPI
+import com.kotlincocktail.hotpepperapi.view.DetailView
+import com.kotlincocktail.hotpepperapi.view.ResultView
+import com.kotlincocktail.hotpepperapi.view.SearchView
+
+@Composable
+fun MainNavigation(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = "search"
+) {
+    var result: HotpepperAPI.Results? = null
+    var selectedData: HotpepperAPI.Shop? = null
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = "search") {
+            SearchView(
+                navController = navController
+            ){
+                result = it
+                navController.navigate("result")
+            }
+        }
+        composable(route = "result") {
+            ResultView(
+                navController = navController,
+                data = result
+            ){
+                selectedData = it
+                navController.navigate("detail")
+            }
+        }
+        composable(route = "detail") {
+            selectedData?.let { it1 ->
+                DetailView(
+                    navController = navController,
+                    data = it1
+                )
+            }
+        }
+    }
+}
