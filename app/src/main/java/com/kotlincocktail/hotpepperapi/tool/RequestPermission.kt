f9a9e8f20901e842cf437411fa37fb58a10bfbec
Y_Nishino <kaek33586@gmail.com>
2024-01-22 08:17:19 +0900

    add request permission class

--- app/src/main/java/com/kotlincocktail/hotpepperapi/tool/RequestPermission.kt
+++ app/src/main/java/com/kotlincocktail/hotpepperapi/tool/RequestPermission.kt
@@ -1,24 +1,39 @@
 package com.kotlincocktail.hotpepperapi.tool
 
 import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.core.content.ContextCompat
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
 
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-fun RequestLocationPermission(hasPermission:  () -> Unit = {}) {
-    val permissionState = rememberPermissionState(ACCESS_FINE_LOCATION)
-    when {
-        permissionState.hasPermission -> hasPermission
-        permissionState.shouldShowRationale -> SideEffect {
-            //TODO リクエストを要求する理由
-            permissionState.launchPermissionRequest()
+
+class RequestPermission(
+    private val activity: ComponentActivity,
+    private val hasPermission: () -> Unit
+) {
+
+    private val requestPermissionLauncher =
+        activity.registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                hasPermission()
+            }
         }
-        permissionState.permissionRequested -> {}
-        else -> SideEffect {
-            permissionState.launchPermissionRequest()
+
+    fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                activity,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        ) {
+            hasPermission()
+        } else {
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
 }
