56e904bdb8f9361bef14ce615dad567215d72a62
Y_Nishino <kaek33586@gmail.com>
2024-01-22 08:16:31 +0900

    add navigation to set content

--- app/src/main/java/com/kotlincocktail/hotpepperapi/MainActivity.kt
+++ app/src/main/java/com/kotlincocktail/hotpepperapi/MainActivity.kt
@@ -7,21 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.kotlincocktail.hotpepperapi.tool.RequestLocationPermission
+import com.kotlincocktail.hotpepperapi.navigation.MainNavigation
 import com.kotlincocktail.hotpepperapi.ui.theme.HotpepperAPITheme
-import com.kotlincocktail.hotpepperapi.view.SearchView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RequestLocationPermission()
             HotpepperAPITheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SearchView()
+                    MainNavigation()
                 }
             }
         }
