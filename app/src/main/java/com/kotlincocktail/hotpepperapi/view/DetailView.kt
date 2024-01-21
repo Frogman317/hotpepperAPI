032177cdc09caa0784c64b9a1dddd6d4d2ada37c
Y_Nishino <kaek33586@gmail.com>
2024-01-22 08:13:20 +0900

    add detail view

---
+++ app/src/main/java/com/kotlincocktail/hotpepperapi/view/DetailView.kt
@@ -0,0 +1,13 @@
+package com.kotlincocktail.hotpepperapi.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.kotlincocktail.hotpepperapi.tool.HotpepperAPI
+
+@Composable
+fun DetailView(
+    navController: NavController,
+    data: HotpepperAPI.Shop
+) {
+
+}
