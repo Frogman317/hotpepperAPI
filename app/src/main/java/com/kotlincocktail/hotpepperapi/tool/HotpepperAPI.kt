852c209d56c76107e41fe9b6561b42369e47f72f
Y_Nishino <kaek33586@gmail.com>
2024-01-22 08:14:37 +0900

    add fetch data class

--- app/src/main/java/com/kotlincocktail/hotpepperapi/tool/HotpepperAPI.kt
+++ app/src/main/java/com/kotlincocktail/hotpepperapi/tool/HotpepperAPI.kt
@@ -1,31 +1,51 @@
 package com.kotlincocktail.hotpepperapi.tool
 
-import java.net.URL
-import java.security.cert.Certificate
-import javax.net.ssl.HttpsURLConnection
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-class HotpepperAPI(url: URL?) : HttpsURLConnection(url) {
-    override fun connect() {
-        TODO("Not yet implemented")
-    }
+interface HotpepperAPI{
+    @GET("/hotpepper/gourmet/v1")
+    suspend fun getGourmet(
+        @Query("key") key: String = "40b28d2a29eb23a3",
+        @Query("format") format: String = "json",
+        @Query("type") type: String = "lite",
+        @Query("keyword") keyword: String = "",
+        @Query("lat") lat: String = "35.669220",
+        @Query("lng") lng: String = "139.761457",
+    ): Response<GourmetData>
+    data class GourmetData(
+        val results: Results
+    )
+    data class Results(
+        val results_returned: String,
+        val shop: List<Shop>,
+    )
+    data class Shop(
+        val name: String,
+        val address: String,
+        val lat: Float,
+        val lng: Float,
+        val genre: Genre,
+        val catch: String,
+        val access: String,
+        val urls: Urls,
+        val photo: Photo
+    )
 
-    override fun disconnect() {
-        TODO("Not yet implemented")
-    }
-
-    override fun usingProxy(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun getCipherSuite(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getLocalCertificates(): Array<Certificate> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getServerCertificates(): Array<Certificate> {
-        TODO("Not yet implemented")
-    }
+    data class Genre(
+        val name: String,
+        val catch: String,
+    )
+    data class Urls(
+        val pc: String
+    )
+    data class Photo(
+        val pc: PhotoData
+    )
+    data class PhotoData(
+        val l: String,
+        val m: String,
+        val s: String
+    )
 }
